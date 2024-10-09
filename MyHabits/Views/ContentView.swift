96e686deb65c0ee24de970f829ba1473414c0ffa
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-09 19:22:00 +0200

    Updated ContentView

--- MyHabits/Views/ContentView.swift
+++ MyHabits/Views/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var storageManager = StorageManager()
+    @State private var showingAddActivity = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(storageManager.habits) { habit in
+                    NavigationLink(destination: HabitDetailView(habit: habit, storageManager: storageManager)) {
+                        VStack(alignment: .leading) {
+                            Text(habit.title)
+                                .font(.headline)
+                            Text(habit.description)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Habit Tracker")
+            .toolbar {
+                Button("Add", systemImage: "plus") {
+                    showingAddActivity = true
+                }
+            }
+            .sheet(isPresented: $showingAddActivity) {
+                AddHabitView(storageManager: storageManager)
+            }
         }
-        .padding()
     }
 }
 
