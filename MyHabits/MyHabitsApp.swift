5ee6b0639314ea9ca7e1ad525e6ee863851abd43
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-09 18:40:00 +0200

    Initial Commit

---
+++ MyHabits/MyHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitsApp.swift
+//  MyHabits
+//
+//  Created by Иван Семикин on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
